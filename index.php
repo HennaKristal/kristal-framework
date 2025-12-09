9e300e64f0839037944ceeb4bedea684b484d68e
HennaKristal <hennakristal@outlook.com>
2025-12-10 01:10:36 +0200

    htaccess improvements

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define("WEBROOT", __DIR__);
 
 if (!file_exists(WEBROOT . "/App/Backend/Core/Initialize.php"))
 {
-    exit("Can not find file 'App/Backend/Core/Initialize.php'");
+    exit("Could not load framework core, please check index.php file.");
 }
 
 require_once WEBROOT . "/App/Backend/Core/Initialize.php";
